adee76750fecc00b00a2193d13e6cb2f2caa3272
sruagrawal-UPenn <sru28@seas.upenn.edu>
2025-07-21 19:54:35 -0500

    Add files via upload

--- submitProof.py
+++ submitProof.py
@@ -174,7 +174,7 @@ def send_signed_msg(proof, random_leaf):
         'chainId': 97
     })
 
-    signed_tx = w3.eth.account.sign_transaction(tx, acct.key)
+    signed_tx = acct.sign_transaction(tx)
     tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
     return tx_hash
